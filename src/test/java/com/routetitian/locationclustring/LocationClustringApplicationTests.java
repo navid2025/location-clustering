182cf1df104c2baebeb7ed8d095a236dab2b13e3
n.zare <n.zare@behsacorp.com>
2024-02-22 10:29:14 +0330

    first commit

---
+++ src/test/java/com/routetitian/locationclustring/LocationClustringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.routetitian.locationclustring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationClustringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
